3275123b4dccaf86d81aa85e3c6d540397ccef2f
Aditya Pratap <adityapratap7344@gmail.com>
2020-02-27 00:14:20 +0530

    added _redirects file in public folder and using BrowserRouter instead of HashRouter

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import { PersistGate } from 'redux-persist/integration/react';
@@ -13,11 +13,11 @@ import { store, persistor } from './redux/store';
 
 ReactDOM.render(
     <Provider store={store}>
-        <HashRouter>
+        <BrowserRouter>
             <PersistGate persistor={persistor}>
                 <App />
             </PersistGate>
-        </HashRouter>
+        </BrowserRouter>
     </Provider>
     , document.getElementById('root'));
 
